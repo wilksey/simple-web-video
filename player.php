fecc48906ae5b0a3b85afe0e8d5b0396eabcc4ba
wayne.workman2012@gmail.com <wayne.workman2012@gmail.com>
2017-01-15 09:55:55 -0600

    Added basic titles and headers

--- player.php
+++ player.php
@@ -2,12 +2,18 @@
 
 $file = $_REQUEST['file'];
 $ext = $_REQUEST['ext'];
-$description = pathinfo($file, PATHINFO_DIRNAME) . "/" . pathinfo($file, PATHINFO_FILENAME) . ".description";
+$dirname = pathinfo($file, PATHINFO_DIRNAME);
+$filename = pathinfo($file, PATHINFO_FILENAME);
+$description = $dirname . "/" . $filename . ".description";
 
 
 echo "<html>";
 echo "<head><style> p.description { width: 75em; border: 2px solid #000000; word-wrap: break-word; } </style></head>";
 echo "<body>\n";
+echo "<title>\n";
+echo "$filename\n";
+echo "</title>\n";
+echo "<h3>$filename</h3>\n";
 echo "<video controls autoplay preload=\"auto\" src=\"stream.php?file=$file&ext=$ext\" width=\"60%\"></video>\n";
 echo "<br><br>\n";
 echo "<p class=\"description\">\n";
